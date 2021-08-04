66edc75365d0eb0f4881cae5d6676a151097b3a0
sarahebrooks12 <sarahebrooks12@gmail.com>
2021-08-04 12:25:54 -0400

    HTML functioning

---
+++ FishDataProvider.js
@@ -0,0 +1,10 @@
+const fishCollection = [
+    {
+        image: "https://tinyurl.com/2nthw5y5",
+        type: "Goldie",
+        length: "2 inches",
+        name: "Goldie",
+        harvested: "Your mom's pond",
+        food: "Dreams"
+    }
+]
